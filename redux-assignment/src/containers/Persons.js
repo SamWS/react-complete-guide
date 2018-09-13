a60bdd8fd7c18de64aaa54475d8b7f6a1be4bd77
Sam West-Sooby <s.west.sooby@gmail.com>
2018-09-13 22:19:02 +1000

    first commit of redux assignment

---
+++ redux-assignment/src/containers/Persons.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+class Persons extends Component {
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {this.props.persons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onDeletePerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+  return {
+    persons: state.persons,
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPerson: () => dispatch({ type: 'ADD_PERSON' }),
+    onDeletePerson: (id) => dispatch({ type: 'DELETE_PERSON', id: id})
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
---
+++ redux-assignment/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const initialState = {
+  persons: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_PERSON':
+      const newPerson = {
+          id: Math.random(), // not really unique but good enough here!
+          name: 'Max',
+          age: Math.floor( Math.random() * 40 )
+      }
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson)
+      }  
+    case 'DELETE_PERSON':
+      return { 
+        ...state,
+        persons: state.persons.filter(person => person.id !== action.id)
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
+registerServiceWorker();
